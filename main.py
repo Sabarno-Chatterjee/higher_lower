71e6b7031b271484cf12e357684d40a820563b68
Sabarno Chatterjee <sabarno.05@gmail.com>
2022-11-26 02:55:49 +0530

    Edit main.py

--- main.py
+++ main.py
@@ -7,12 +7,28 @@ print(art.logo)
 first = random.choice(game_data.data)
 second = random.choice(game_data.data)
 
-
 first_position = game_data.data.index(first)
 second_position = game_data.data.index(second)
 
 print(first["name"])
 
-print(f"\n\nCompare A: {first['name']}, a {first['description']}, from {first['country']}.\n\n")
+print(
+    f"\n\nCompare A: {first['name']}, a {first['description']}, from {first['country']}.\n\n"
+)
 print(art.vs)
-print(f"\n\nCompare A: {second['name']}, a {second['description']}, from {second['country']}.\n\n")
+print(
+    f"\n\nCompare A: {second['name']}, a {second['description']}, from {second['country']}.\n\n"
+)
+
+result = "b"
+
+if first["follower_count"] > second["follower_count"]:
+    result = "a"
+
+score = 0
+choice = input("Who has more followers? Type 'A' or 'B':").lower()
+if choice == result:
+    print(f"\nYou are correct. Your score is {score}\n")
+    score += 1
+else:
+    print(f"\n Sorry, you are wrong!. Your final score is {score}\n")
