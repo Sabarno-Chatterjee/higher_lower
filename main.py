0667d0075608ae792e0d52ca369905e219774dff
Sabarno Chatterjee <sabarno.05@gmail.com>
2022-11-26 16:17:32 +0530

    Edit main.py and art.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import art
 import game_data
 
 
-
-
-# first = random.choice(game_data.data)
-# second = random.choice(game_data.data)
 def select():
     """To pick a random list element from game_data.data"""
     return random.choice(game_data.data)
@@ -15,16 +11,12 @@ def select():
 
 first = select()
 second = select()
-# print(first)
-# print(second)
-# first_position = game_data.data.index(first)
-# second_position = game_data.data.index(second)
 
-# print(first["name"])
 is_correct = True
 score = 0
+print(art.logo)
 while is_correct:
-  print(art.logo)
+  
   print(
       f"\n\nCompare A: {first['name']}, a {first['description']}, from {first['country']}.\n\n"
   )
@@ -49,9 +41,10 @@ while is_correct:
   
   choice = input("Who has more followers? Type 'A' or 'B'\n").lower()
   if choice == result:
-      score += 1
-      print(f"\nYou are correct. Your score is {score}\n")
       replit.clear()
+      score += 1
+      print(f"\n\n\nYou are correct. Your score is {score}\n")
+      
   
   else:
       print(f"\n Sorry, you are wrong!. Your final score is {score}\n")
